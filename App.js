c707e0265ad9bd1de8c97112dded8572252e99fd
NicoleLatifi <Nicole.Latifi@gmail.com>
2021-09-23 20:18:58 -0600

    create style sheet

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-  <View style={{ padding: 50 }}>
-    <Text>Hello World</Text>
-    <Text>Red</Text>
-    <Text>Green</Text>
-    <Text>Blue</Text>
-  </View>
-  )
+    <View style={styles.page}>
+      <Text style={styles.text}>red</Text>
+      <Text style={[styles.text, styles.selectedText]}>green</Text>
+      <Text style={styles.text}>blue</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "#DDD",
+    marginTop: 40,
+  },
+  text: {
+    backgroundColor: "yellow",
+    color: "red",
+    fontSize: 22,
+    margin: 10,
+    padding: 5,
+  },
+  selectedText: {
+    backgroundColor: "red",
+    color: "yellow",
+  },
+});
